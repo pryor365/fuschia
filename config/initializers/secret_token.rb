0ee564f041b0889f80c365798990fbbd574ed2f9
Sally <sally.pryor@outlook.com>
2015-01-16 20:43:26 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fuschia::Application.config.secret_key_base = '6f40e2eb472c32a8fd195e03c4c83cbb8500b5a98ab6764619f7347d58e21384e65a7adf06a38d5819ce29207de2711ea925dcc4f80fef6314150875f702e4de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fuschia::Application.config.session_store :cookie_store, key: '_fuschia_session'
